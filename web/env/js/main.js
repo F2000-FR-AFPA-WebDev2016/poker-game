6ed66f1e54b4c4313130d5a206b5c123ad781a1c
fred <webmaster@tootye.com>
2016-05-27 14:00:33 +0200

    ajout de la page acceuil

---
+++ web/env/js/main.js
@@ -0,0 +1,40 @@
+
+(function($) {
+
+    $(function() {
+        
+        var	$window = $(window),
+                $body = $('body'),
+                $first = $('#firstImg'),
+                $second = $('#secondImg'),
+                $tabAllu = [1000, 
+                    20,20,20,20,20,20,20, 
+                    600, 200, 200,
+                    20,20,20,20,20,20,20,20,20,20,
+                    700, 100],
+                $current = 0;
+        
+        $window.on('load', function() {
+            window.setTimeout(function() {
+                    $body.addClass('is-loading');
+                    $first.removeClass('loading');
+                    loadImage();
+            }, 800);
+        });
+        
+        function loadImage(){
+            $.each($tabAllu, function(){
+                $current = $current + this;
+                window.setTimeout(function() {
+                    $second.toggleClass('loading');
+                }, $current);
+            });
+        }
+        
+        
+
+    });
+
+})(jQuery);
+
+
